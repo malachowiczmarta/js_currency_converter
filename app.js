b8195f7ad6d0314502a25c892fe776b13297317c
Marta Maachowicz <malachowicz.marta@gmail.com>
2020-10-04 16:02:35 +0200

    final version

--- app.js
+++ app.js
@@ -1,71 +1,104 @@
-// const service = axios.create({
-//   baseURL: "http://api.nbp.pl/api/exchangerates/tables/a/?format=json",
-//   method: "get",
-// });
 let targetCurrency = "";
-console.log(targetCurrency);
 const select = document.getElementById("selectCurrency");
 
-window.onload = () => {
-  loadCurrencyList();
+const getCurrencyList = async () => {
+  try {
+    return await axios.get(
+      "http://api.nbp.pl/api/exchangerates/tables/a/?format=json"
+    );
+  } catch (error) {
+    console.error(error);
+  }
 };
 
-function loadCurrencyList() {
-  const getCurrencyList = async () => {
-    try {
-      return await axios.get(
-        "http://api.nbp.pl/api/exchangerates/tables/a/?format=json"
-      );
-    } catch (error) {
-      console.error(error);
-    }
-  };
+const getTodayCurrencyValue = async (codetoLowerCase) => {
+  try {
+    return await axios.get(
+      `http://api.nbp.pl/api/exchangerates/rates/a/${codetoLowerCase}/today/`
+    );
+  } catch (error) {
+    console.error(error);
+    return error.message;
+  }
+};
 
-  const currencyList = async () => {
-    const currencies = await getCurrencyList();
-    console.log(currencies.data[0]["rates"]);
-    let codeArr = currencies.data[0]["rates"];
-    codeArr.forEach((element) => {
-      let select = document.getElementById("selectCurrency");
-      let option = document.createElement("option");
-      select.appendChild(option);
-      option.innerText = element.code;
-      option.value = element.code;
-    });
-  };
-  currencyList();
-}
+const getLastCurrencyValue = async (codetoLowerCase) => {
+  try {
+    return await axios.get(
+      `http://api.nbp.pl/api/exchangerates/rates/a/${codetoLowerCase}/`
+    );
+  } catch (error) {
+    console.error(error);
+    return error.message;
+  }
+};
 
-function onCountBtnClick() {
-  console.log(targetCurrency);
-  let codetoLowerCase = targetCurrency.toLocaleLowerCase();
-  console.log(codetoLowerCase);
-  const getCurrencyValue = async () => {
-    try {
-      return await axios.get(
-        `http://api.nbp.pl/api/exchangerates/rates/a/${codetoLowerCase}/?format=json`
-      );
-    } catch (error) {
-      console.error(error);
-    }
-  };
+const loadCurrencyCodeList = async () => {
+  const currencies = await getCurrencyList();
+  let codeArr = currencies.data[0]["rates"];
+  codeArr.forEach((element) => {
+    let select = document.getElementById("selectCurrency");
+    let option = document.createElement("option");
+    select.appendChild(option);
+    option.innerText = element.code;
+    option.value = element.code;
+  });
+};
 
-  const multiplyValue = async () => {
-    const inputMoney = document.getElementById("money-value");
-    const inputMoneyValue = inputMoney.value;
-    const response = await getCurrencyValue();
-    console.log(response.data.rates[0]["mid"]);
+const multiplyTodayValue = async (codetoLowerCase) => {
+  const inputMoney = document.getElementById("money-value");
+  const inputMoneyValue = inputMoney.value;
+  const response = await getTodayCurrencyValue(codetoLowerCase);
+  const para = document.getElementById("para-result");
+  const div = document.querySelector(".para-container");
+  if (response === "Request failed with status code 404") {
+    para.innerText = `Kurs walut nie został dziś jeszcze opublikowany. Czy przeliczyć według ostatniego kursu?`;
+    const acceptBtn = document.createElement("button");
+    acceptBtn.id = "acceptBtn";
+    acceptBtn.innerText = "Tak";
+    acceptBtn.setAttribute("onclick", "onAcceptBtnClick()");
+    div.appendChild(acceptBtn);
+  } else {
     const currencyValue = response.data.rates[0]["mid"];
     const multiply = currencyValue * inputMoneyValue;
 
     const twoDecimals = multiply.toFixed(2);
 
-    const para = document.getElementById("para-result");
     para.innerText = `to ${twoDecimals} PLN`;
-  };
+  }
+};
 
-  multiplyValue();
+const multiplyLastValue = async (codetoLowerCase) => {
+  const inputMoney = document.getElementById("money-value");
+  const inputMoneyValue = inputMoney.value;
+  const response = await getLastCurrencyValue(codetoLowerCase);
+
+  const paraError = document.getElementById("para-result");
+
+  const currencyValue = response.data.rates[0]["mid"];
+  const multiply = currencyValue * inputMoneyValue;
+
+  const twoDecimals = multiply.toFixed(2);
+
+  paraError.innerText = `to ${twoDecimals} PLN`;
+};
+
+window.onload = () => {
+  loadCurrencyCodeList();
+};
+
+function onCountBtnClick() {
+  let codetoLowerCase = targetCurrency.toLowerCase();
+  multiplyTodayValue(codetoLowerCase);
 }
+
+function onAcceptBtnClick() {
+  let codetoLowerCase = targetCurrency.toLowerCase();
+  const yesBtn = document.getElementById("acceptBtn");
+  yesBtn.remove();
+  multiplyLastValue(codetoLowerCase);
+}
+
 select.addEventListener("change", (event) => {
   targetCurrency = event.target.value;
 });
