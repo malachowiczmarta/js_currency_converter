249fad4d3e2baa9c605890918609be7b2977dd0e
Marta Maachowicz <malachowicz.marta@gmail.com>
2020-10-08 13:34:22 +0200

    remove getCurrency function, add multiply function

--- app.js
+++ app.js
@@ -1,86 +1,68 @@
 let targetCurrency = "";
 const select = document.getElementById("selectCurrency");
 
-const getCurrencyList = async () => {
+const loadCurrencyCodeList = async () => {
   try {
-    return await axios.get(
+    const response = await axios.get(
       "http://api.nbp.pl/api/exchangerates/tables/a/?format=json"
     );
+
+    let codeArr = response.data[0]["rates"];
+    codeArr.forEach((element) => {
+      let select = document.getElementById("selectCurrency");
+      let option = document.createElement("option");
+      select.appendChild(option);
+      option.innerText = element.code;
+      option.value = element.code;
+    });
   } catch (error) {
     console.error(error);
   }
 };
 
-const getTodayCurrencyValue = async (codetoLowerCase) => {
-  try {
-    return await axios.get(
-      `http://api.nbp.pl/api/exchangerates/rates/a/${codetoLowerCase}/today/`
-    );
-  } catch (error) {
-    console.error(error);
-    return error.message;
-  }
-};
+const multiplyValue = (response, inputMoneyValue) => {
+  const currencyValue = response.data.rates[0]["mid"];
+  const multiply = currencyValue * inputMoneyValue;
 
-const getLastCurrencyValue = async (codetoLowerCase) => {
-  try {
-    return await axios.get(
-      `http://api.nbp.pl/api/exchangerates/rates/a/${codetoLowerCase}/`
-    );
-  } catch (error) {
-    console.error(error);
-    return error.message;
-  }
-};
-
-const loadCurrencyCodeList = async () => {
-  const currencies = await getCurrencyList();
-  let codeArr = currencies.data[0]["rates"];
-  codeArr.forEach((element) => {
-    let select = document.getElementById("selectCurrency");
-    let option = document.createElement("option");
-    select.appendChild(option);
-    option.innerText = element.code;
-    option.value = element.code;
-  });
+  const twoDecimals = multiply.toFixed(2);
+  return `to ${twoDecimals} PLN`;
 };
 
 const multiplyTodayValue = async (codetoLowerCase) => {
+  const paragraph = document.getElementById("para-result");
+  const div = document.querySelector(".para-container");
   const inputMoney = document.getElementById("money-value");
   const inputMoneyValue = inputMoney.value;
-  const response = await getTodayCurrencyValue(codetoLowerCase);
-  const para = document.getElementById("para-result");
-  const div = document.querySelector(".para-container");
-  if (response === "Request failed with status code 404") {
-    para.innerText = `Kurs walut nie został dziś jeszcze opublikowany. Czy przeliczyć według ostatniego kursu?`;
+
+  try {
+    const response = await axios.get(
+      `http://api.nbp.pl/api/exchangerates/rates/a/${codetoLowerCase}/today/`
+    );
+
+    paragraph.innerText = multiplyValue(response, inputMoneyValue);
+  } catch (error) {
+    paragraph.innerText = `Kurs walut nie został dziś jeszcze opublikowany. Czy przeliczyć według ostatniego kursu?`;
+
     const acceptBtn = document.createElement("button");
     acceptBtn.id = "acceptBtn";
     acceptBtn.innerText = "Tak";
     acceptBtn.setAttribute("onclick", "onAcceptBtnClick()");
     div.appendChild(acceptBtn);
-  } else {
-    const currencyValue = response.data.rates[0]["mid"];
-    const multiply = currencyValue * inputMoneyValue;
-
-    const twoDecimals = multiply.toFixed(2);
-
-    para.innerText = `to ${twoDecimals} PLN`;
   }
 };
 
 const multiplyLastValue = async (codetoLowerCase) => {
+  const paragraph = document.getElementById("para-result");
   const inputMoney = document.getElementById("money-value");
   const inputMoneyValue = inputMoney.value;
-  const response = await getLastCurrencyValue(codetoLowerCase);
-
-  const paraError = document.getElementById("para-result");
-
-  const currencyValue = response.data.rates[0]["mid"];
-  const multiply = currencyValue * inputMoneyValue;
-
-  const twoDecimals = multiply.toFixed(2);
-
-  paraError.innerText = `to ${twoDecimals} PLN`;
+  try {
+    const response = await axios.get(
+      `http://api.nbp.pl/api/exchangerates/rates/a/${codetoLowerCase}/`
+    );
+    paragraph.innerText = multiplyValue(response, inputMoneyValue);
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 window.onload = () => {
